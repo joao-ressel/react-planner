6d288ea6365f1d01651d61f03d86ca1b46feffee
joao-ressel <joao.resselti@gmail.com>
2024-07-10 23:46:49 -0500

    refactor: fazendo o cadastro e a listagem de atividades da viagem na pagina de detalhes

--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,13 +1,44 @@
-import { Calendar, Tag, Watch, X } from "lucide-react";
+import { Calendar, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
 
 interface CreateActivityModalProps {
   closeModal: () => void;
 }
 
-export const CreateActivityModal = ({
-  closeModal,
-}: CreateActivityModalProps) => {
+export const CreateActivityModal = ({ closeModal }: CreateActivityModalProps) => {
+  const { tripId } = useParams();
+
+  async function createActivity(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+
+    const title = data.get("title")?.toString();
+    const occurs_at = data.get("occurs_at")?.toString();
+
+    console.log("Form data:", { title, occurs_at });
+
+    if (!title || !occurs_at) {
+      console.error("Title or occurs_at is missing.");
+      return;
+    }
+
+    try {
+      const response = await api.post(`/trips/${tripId}/activities`, {
+        title,
+        occurs_at,
+      });
+      console.log("API Response:", response.data);
+      closeModal();
+    } catch (error) {
+      console.error("API Error:", error);
+    }
+
+    window.document.location.reload()
+  }
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -18,43 +49,33 @@ export const CreateActivityModal = ({
               <X className="size-5 text-zinc-400" />
             </Button>
           </div>
-          <p className="text-sm text-zinc-400">
-            Todos convidados podem visualizar as atividades.
-          </p>
+          <p className="text-sm text-zinc-400">Todos convidados podem visualizar as atividades.</p>
         </div>
-
-        <div className="w-full h-px bg-zinc-800" />
-        <form className=" space-y-3">
+        <form onSubmit={createActivity} className="space-y-3">
           <div className="px-2.5 h-14 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
             <Tag className="text-zinc-400 size-5" />
             <input
               name="title"
               placeholder="Qual a atividade?"
               className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+              required
             />
           </div>
-          <div className="flex items-center gap-2">
-            <div className="px-2.5 h-14 flex-1 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-              <Calendar className="text-zinc-400 size-5" />
-              <input
-                type="date"
-                name="at_date"
-                placeholder="Data da atividade"
-                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1 [color-scheme:dark]"
-              />
-            </div>
-            <div className="px-4 h-14 w-36 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-              <Watch className="text-zinc-400 size-5" />
-              <input
-                type="time"
-                name="at_hour"
-                placeholder="Horário"
-                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1 [color-scheme:dark]"
-              />
-            </div>
+
+          <div className="px-4 h-14 flex-1 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <Calendar className="text-zinc-400 size-5" />
+            <input
+              type="datetime-local"
+              name="occurs_at"
+              placeholder="Data e hora da atividade"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1 [color-scheme:dark]"
+              required
+            />
           </div>
+          <Button size="full" type="submit">
+            Salvar atividade
+          </Button>
         </form>
-        <Button type="button">Salvar atividade</Button>
       </div>
     </div>
   );
